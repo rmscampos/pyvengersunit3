1a33c6b0c5f91030c02e326c2826a94d9da3285d
Finn <anthony.torres0926@gmail.com>
2019-12-31 17:10:32 -0600

    Changed cats to concerts.

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render, redirect
-<<<<<<< HEAD
-from django.views.generic.edit import CreateView
-=======
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
->>>>>>> 7ca05a3393c48be4b7bb73b012f5a3c1cbc27b43
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import ListView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -28,16 +24,10 @@ class ConcertUpdate(LoginRequiredMixin, UpdateView):
   model = Concert
   fields = ['artist', 'location', 'date', 'time']
 
-<<<<<<< HEAD
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'pyvengersunit3'
 
 # Create your views here.
-=======
-class ConcertDelete(LoginRequiredMixin, DeleteView):
-  model = Concert
-  success_url = '/concerts/'
->>>>>>> 7ca05a3393c48be4b7bb73b012f5a3c1cbc27b43
 
 def home(request):
   return render(request, 'home.html')
@@ -50,14 +40,6 @@ def concerts_index(request):
   concerts = Concert.objects.filter(user=request.user)
   return render(request, 'concerts/index.html', { 'concerts' : concerts })
 
-<<<<<<< HEAD
-=======
-@login_required
-def concerts_detail(request, concert_id):
-  concert = Concert.objects.get(id=concert_id)
-  return render(request, 'concerts/detail.html', { 'concert' : concert })
-
->>>>>>> 7ca05a3393c48be4b7bb73b012f5a3c1cbc27b43
 def signup(request):
   error_message = ''
   if request.method == 'POST':
@@ -80,8 +62,8 @@ def add_photo(request, cat_id):
         try:
             s3.upload_fileobj(photo_file, BUCKET, key)
             url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            photo = Photo(url=url, cat_id=cat_id)
+            photo = Photo(url=url, concerts_index=concerts_index)
             photo.save()
         except:
             print('An error occurred uploading file to S3')
-    return redirect('detail', cat_id=cat_id)
+    return redirect('detail', concerts_index=concerts_index)
