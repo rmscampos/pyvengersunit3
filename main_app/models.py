18c5eaccd9127a0a32e1fe5195061d6be6e75685
Finn <anthony.torres0926@gmail.com>
2020-01-02 21:13:59 -0600

    Moved Photos. Working Properly.

--- main_app/models.py
+++ main_app/models.py
@@ -43,7 +43,7 @@ class Concert(models.Model):
 
 class Photo(models.Model):
     url = models.CharField(max_length=200)
-    concert = models.ForeignKey(Review, on_delete=models.CASCADE)
+    concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Photo for concert_id: {self.concert_id} @{self.url}"
